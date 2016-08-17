76111ff54870078ea41715db58aaacd9dbb9891d
sadamo <daniel.sadamo@kmee.com.br>
2016-08-16 21:22:08 -0300

    [NEW] Module that links the pos order with it respective picking
    FIX typo

--- pos_order_picking_link/models/stock.py
+++ pos_order_picking_link/models/stock.py
@@ -2,12 +2,12 @@
 # © 2016 KMEE(<http://www.kmee.com.br>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models, _
+from openerp import fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     pos_order_ids = fields.One2many(comodel_name='pos.order',
-                                   inverse_name='picking_id', copy=False,
-                                   string='POS order', readonly=True)
+                                    inverse_name='picking_id', copy=False,
+                                    string='POS order', readonly=True)
