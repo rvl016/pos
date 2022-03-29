7f73abb6e85fb27799722737b4a33943dade043f
Luis Felipe Mileo <mileo@kmee.com.br>
2022-03-29 20:07:42 -0300

    [NEW] Pos PouchDB

---
+++ pos_pouchdb/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Pos Pouchdb',
+    'summary': """
+        Point Of Sale PouchDB Storage""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/pos',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+}
---
+++ pos_pouchdb/static/src/js/custom.js
@@ -0,0 +1,75 @@
+odoo.define("pos_pouchdb.db", function (require) {
+    "use strict";
+    var PosDB = require("point_of_sale.DB");
+    var models = require('point_of_sale.models');
+
+    PosDB.include({
+        init: function (options) {
+            this._super.apply(this, arguments);
+            this.p_db = {};
+            this.pouch_db = new PouchDB('http://admin:admin@localhost:5984/unpaid_orders');
+        },
+
+        save_unpaid_order: function (order) {
+            var self = this;
+            try {
+                var serialized = order.export_as_JSON();
+                self.pouch_db.info().then(function (info) {
+                    console.log(info);
+                });
+                self.pouch_db.get(serialized._id).then(function(doc) {
+                    serialized._rev = doc._rev
+                    self.pouch_db.put(serialized).then(function(doc){
+                        return serialized.uid;
+                    });
+                }).catch(function (err) {
+                    self.pouch_db.put(serialized).then(function(doc){
+                        return serialized.uid;
+                    });
+                });
+            } catch (e) {
+                return this._super.apply(this, arguments);
+            }
+        },
+        remove_unpaid_order: function (order) {
+            try {
+                var serialized = order.export_as_JSON();
+                this.pouch_db.get(serialized._id).then(function(doc) {
+                  this.pouch_db.remove(doc);
+                }).then(function (result) {
+                  // handle result
+                }).catch(function (err) {
+                  this._super.apply(this, arguments);
+                });
+            } catch (e) {
+                this._super.apply(this, arguments);
+            }
+        },
+        remove_all_unpaid_orders: function () {
+            this._super.apply(this, arguments);
+        },
+        get_unpaid_orders: async function () {
+            var self = this;
+            try {
+              var result = await self.pouch_db.allDocs({
+                include_docs: true,
+                attachments: true
+              });
+              return result.rows;
+            } catch (err) {
+              return [];
+            }
+        },
+    });
+  const _super_order = models.Order.prototype;
+
+  models.Order = models.Order.extend({
+    export_as_JSON: function () {
+      const json = _super_order.export_as_JSON.call(this);
+      json._id = this.uid;
+      return json;
+    },
+  });
+
+});
+
